c9d4f15c8159ee54fcd32e7a123d3c8490b79473
JaylinLee <apx103@foxmail.com>
2024-03-05 17:37:27 +0800

    [dev]

--- hello_world.py
+++ hello_world.py
@@ -1,13 +1,8 @@
-# -*- coding: utf-8 -*-
 # @FileName : hello_world.py
-# @License :  (C) Copyright 2023-2024, PJLAB 
+# @License :  (C) Copyright 2023-2024, PJLAB
 # @Time :     2024/03/05 14:35:04
 # @Author :   APX103
 
-
-print
-("Hello
- 
-  World")
-print("Hello World")
-print("Hello World")
+print('Hello World')
+print('Hello World')
+print('Hello World')
